39bbd923a11c6d53a796dfd5481c856c5c94bcb6
ililililililililililililililil <87892213+ililililililililililililililil@users.noreply.github.com>
2021-07-27 11:40:10 +0900

    버튼 구현
    폰트랑 디자인도 찾아야 할 것 같아요

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DontDestroy : MonoBehaviour
 {
     //¸ñ¼û(°è¶õ)
-    public static int eggHp;
+    public static int eggHp = 5;
 
     public GameObject egg1;
     public GameObject egg2;
@@ -16,21 +16,61 @@ public class DontDestroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (eggHp == 4)
-            egg1.SetActive(false);
-        else if (eggHp == 3)
-            egg2.SetActive(false);
-        else if (eggHp == 2)
-            egg3.SetActive(false);
-        else if (eggHp == 1)
-            egg4.SetActive(false);
-        else
+        if (eggHp == 5)
+        {
+            egg1.SetActive(true);
+            egg2.SetActive(true);
+            egg3.SetActive(true);
+            egg4.SetActive(true);
+            egg5.SetActive(true);
+        }
+        else if (eggHp == 4)
+        {
+            egg1.SetActive(true);
+            egg2.SetActive(true);
+            egg3.SetActive(true);
+            egg4.SetActive(true);
             egg5.SetActive(false);
+        }
+        else if (eggHp == 3)
+        {
+            egg1.SetActive(true);
+            egg2.SetActive(true);
+            egg3.SetActive(true);
+            egg4.SetActive(false);
+            egg5.SetActive(false);
+        }
+        else if (eggHp == 2)
+        {
+            egg1.SetActive(true);
+            egg2.SetActive(true);
+            egg3.SetActive(false);
+            egg4.SetActive(false);
+            egg5.SetActive(false);
+        }
+        else if (eggHp == 1)
+        {
+            egg1.SetActive(true);
+            egg2.SetActive(false);
+            egg3.SetActive(false);
+            egg4.SetActive(false);
+            egg5.SetActive(false);
+        }
+        else
+        {
+            egg1.SetActive(false);
+            egg2.SetActive(false);
+            egg3.SetActive(false);
+            egg4.SetActive(false);
+            egg5.SetActive(false);
+
+            DontDestroyOnLoad(gameObject);
+        }
     }
 }
