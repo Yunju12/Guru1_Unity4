0d34686e84c4494f4fb3faf7b8978664c23b88ed
sysysysyb <sunset8670@swu.ac.kr>
2021-07-28 13:44:51 +0900

    shooting0728_1pm

--- Assets/Scripts/BallManager.cs
+++ Assets/Scripts/BallManager.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FireBallManager : MonoBehaviour
+public class BallManager : MonoBehaviour
 {
-    // 파이어볼 공장 변수
-    public GameObject fireBallFactory;
+    // 파이어볼(or 워터볼) 공장 변수
+    public GameObject ballFactory;
 
     // 생성시간 변수
     public float createTime = 2;
@@ -19,24 +19,27 @@ public class FireBallManager : MonoBehaviour
     // 최대 시간 변수
     public float maxTime = 1;
 
-    // 보스 몬스터 컴포넌트 변수
+    // 보스몬스터 컴포넌트 변수
     BossMonster bm;
 
     void Start()
     {
         // 생성시간을 최소 시간과 최대 시간 사이에서 랜덤으로 정한다.
         createTime = Random.Range(minTime, maxTime);
+
+        // 보스몬스터 텀포넌트 가져오기
+        bm = GetComponent<BossMonster>();
     }
 
     void Update()
     {
-        // 게임 상태가 게임 중 상태가 아니면 업데이트 함수를 중단
+        // 게임 상태가 게임 중 상태가 아니면 업데이트 함수를 중단한다.
         if (GameManager.gm.gState != GameManager.GameState.Run)
         {
             return;
         }
 
-        // *일정한 생성시간에 한번씩 적 생성하기.
+        // * 일정한 생성 시간에 한번씩 적 생성하기
         // 1. 경과시간을 잰다.
         currentTime += Time.deltaTime;
 
@@ -44,7 +47,7 @@ public class FireBallManager : MonoBehaviour
         if (currentTime > createTime)
         {
             // 3. 적을 적공장에서 생성한다.
-            GameObject fireball = Instantiate(fireBallFactory);
+            GameObject fireball = Instantiate(ballFactory);
 
             // 4. 생성된 적을 배치한다.
             fireball.transform.position = transform.position;
@@ -54,11 +57,9 @@ public class FireBallManager : MonoBehaviour
             createTime = Random.Range(minTime, maxTime);
         }
 
-        BossMonster bm = GameObject.Find("BossMonster").GetComponent<BossMonster>();
-
-        if (bm.bossHp <= 0)
-        {
-            gameObject.SetActive(false);
-        }
+        //if (bm.bossHp <= 0)
+        //{
+           //gameObject.SetActive(false);
+        //}
     }
 }
