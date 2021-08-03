6974f9828368b9e9e83c53cdd3c55b65549059f6
ililililililililililililililil <87892213+ililililililililililililililil@users.noreply.github.com>
2021-08-03 15:22:01 +0900

    슈팅게임 바닥

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,6 +5,10 @@ using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
 {
+    public GameObject floor1;
+    public GameObject floor2;
+    public GameObject floor3;
+
     // 이동을 위한 변수
     Rigidbody2D rigid;
 
@@ -149,14 +153,16 @@ public class PlayerMove : MonoBehaviour
 
         if (Input.GetButton("Jump"))
         {
-            GetComponent<CircleCollider2D>().isTrigger = true;
+            floor1.GetComponent<BoxCollider2D>().isTrigger = true;
+            floor2.GetComponent<BoxCollider2D>().isTrigger = true;
+            floor3.GetComponent<BoxCollider2D>().isTrigger = true;
         }
         else
         {
-            GetComponent<CircleCollider2D>().isTrigger = false;
+            floor1.GetComponent<BoxCollider2D>().isTrigger = false;
+            floor2.GetComponent<BoxCollider2D>().isTrigger = false;
+            floor3.GetComponent<BoxCollider2D>().isTrigger = false;
         }
-
-        
     }
 
     // 만일 플레이어가 땅에 착지하였다면,
