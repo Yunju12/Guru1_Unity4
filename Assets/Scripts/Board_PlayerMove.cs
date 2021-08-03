7df5377c2c57dd71f7791a3e9c7b94f9fc9f13a1
ililililililililililililililil <87892213+ililililililililililililililil@users.noreply.github.com>
2021-08-03 16:48:58 +0900

    주사위 랜덤으로

--- Assets/Scripts/Board_PlayerMove.cs
+++ Assets/Scripts/Board_PlayerMove.cs
@@ -140,10 +140,10 @@ public class Board_PlayerMove : MonoBehaviour
         NPC_B.SetActive(false);
         NPC_E2.SetActive(false);
 
-        //ran = Random.Range(1, 7);
+        ran = Random.Range(1, 7);
         //print(ran);
 
-        ran = 1;
+        //ran = 1;
 
         if (ran == 1)
         {
